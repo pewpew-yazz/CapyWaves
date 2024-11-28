e6c3fc5d13cf9f427c5c32b87e576c3b7e05a44f
Perla Jaques <0323106289@ut-tijuana.edu.mx>
2024-11-28 09:13:07 -0800

    CapyWaves

--- php/registro.php
+++ php/registro.php
@@ -1,17 +1,16 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+session_start();
 
 include_once 'conexion.php';
 
-function registroUsuario($username, $nombre, $apellido_P, $apellido_M, $correo, $contraseña)
+function registroUsuario($username, $nombre, $apellido_P, $apellido_M, $email, $password)
 {
     // Establecer conexión a la base de datos
     $db = connectdb();
 
     // Preparar la consulta para insertar el nuevo usuario
-    $stmt = $db->prepare("INSERT INTO usuarios (username, nombre, apellido_P, apellido_M, email, password) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $username, $nombre, $apellido_P, $apellido_M, $correo, $contraseña);
+    $stmt = $db->prepare("INSERT INTO usuarios (username, email, password, nombre, apellido_P, apellido_M) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssss", $username, $email, $password, $nombre, $apellido_P, $apellido_M);
 
     // Ejecutar la consulta y verificar si fue exitosa
     if ($stmt->execute()) {
