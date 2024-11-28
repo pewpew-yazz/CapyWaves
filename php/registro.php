88f06a82509e09753cec16c5a12e1ba106810ccb
capy-wave <peww.kay@gmail.com>
2024-11-28 09:25:42 -0800

    Revert "Merge branch 'main' of https://github.com/pewpew-yazz/CapyWaves"
    This reverts commit 3caae5cb5a9deceb16a458c0ee2e95c22eca4b53, reversing
    changes made to a47406b67babac7b13276abc5baa9807b8913213.

--- php/registro.php
+++ php/registro.php
@@ -1,16 +1,17 @@
 <?php
-session_start();
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 include_once 'conexion.php';
 
-function registroUsuario($username, $nombre, $apellido_P, $apellido_M, $email, $password)
+function registroUsuario($username, $nombre, $apellido_P, $apellido_M, $correo, $contraseña)
 {
     // Establecer conexión a la base de datos
     $db = connectdb();
 
     // Preparar la consulta para insertar el nuevo usuario
-    $stmt = $db->prepare("INSERT INTO usuarios (username, email, password, nombre, apellido_P, apellido_M) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $username, $email, $password, $nombre, $apellido_P, $apellido_M);
+    $stmt = $db->prepare("INSERT INTO usuarios (username, nombre, apellido_P, apellido_M, email, password) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssss", $username, $nombre, $apellido_P, $apellido_M, $correo, $contraseña);
 
     // Ejecutar la consulta y verificar si fue exitosa
     if ($stmt->execute()) {
