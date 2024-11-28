c7d348351f42f22e8e01e64f2da7fab2120e4f35
capy-wave <peww.kay@gmail.com>
2024-11-28 01:01:10 -0800

    h

--- php/registro.php
+++ php/registro.php
@@ -2,31 +2,26 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-include 'conexion.php';
+include_once 'conexion.php';
 
-if (isset($_POST["registro"])) {
-    $username = mysqli_real_escape_string($enlace, $_POST["username"]);
-    $nombre = mysqli_real_escape_string($enlace, $_POST["nombre"]);
-    $apellido_P = mysqli_real_escape_string($enlace, $_POST["apellido_paterno"]);
-    $apellido_M = isset($_POST["apellido_materno"]) ? mysqli_real_escape_string($enlace, $_POST["apellido_materno"]) : NULL;
-    $correo = mysqli_real_escape_string($enlace, $_POST["correo"]);
-    $contraseña = password_hash($_POST["contraseña"], PASSWORD_BCRYPT);
+function registroUsuario($username, $nombre, $apellido_P, $apellido_M, $correo, $contraseña)
+{
+    // Establecer conexión a la base de datos
+    $db = connectdb();
 
-    // Insertar datos en la base de datos
-    $sql = "INSERT INTO usuarios (username, nombre, apellido_P, apellido_M, email, password) VALUES (?, ?, ?, ?, ?, ?)";
-    $stmt = $enlace->prepare($sql);
+    // Preparar la consulta para insertar el nuevo usuario
+    $stmt = $db->prepare("INSERT INTO usuarios (username, nombre, apellido_P, apellido_M, email, password) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssss", $username, $nombre, $apellido_P, $apellido_M, $correo, $contraseña);
 
+    // Ejecutar la consulta y verificar si fue exitosa
     if ($stmt->execute()) {
-        header("Location: ../menu.php");
+        header("Location: ../discos.php");
         exit();
     } else {
         echo "Error: " . $stmt->error;
     }
 
     $stmt->close();
-    $enlace->close();
-} else {
-    echo "No se recibieron datos del formulario.";
+    $db->close();
 }
 ?>
